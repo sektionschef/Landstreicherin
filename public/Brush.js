4be665853b11d468832b8140981c91de1731fd95
sektionschef <the.stevee@gmail.com>
2022-09-14 21:12:03 +0200

    trying the bst

--- public/Brush.js
+++ public/Brush.js
@@ -12,10 +12,11 @@ class Brush {
         this.strokeSize = 0.3;
 
         this.orientation = orientation;
-        this.start = start;  // start of line
-        this.end = end;  // end of line
-        this.start2 = start2;  // y axis
-        this.end2 = end2;  // y axis
+        this.start = createVector(start, start2, 0);  // start of line
+        this.end = p5.Vector.add(this.start, createVector(end, end2, 0));  // end of line
+        // this.start2 = start2;  // y axis
+        // this.end2 = end2;  // y axis
+        console.log(this.end);
 
         this.alive = true;
         if (this.orientation == "x") {
@@ -23,26 +24,30 @@ class Brush {
         } else if (this.orientation == "y") {
             this.pos = createVector(0, this.start, 0);
         } else if (this.orientation == "xy") {
-            this.pos = createVector(this.start, this.start2, 0);
+            // this.pos = createVector(this.start, this.start2, 0);
+            this.pos = this.start;
         } else if (this.orientation == "yx") {
             this.pos = createVector(this.start2, this.start, 0);
         }
 
         this.vel = createVector(0, 0, 0);
         this.acc = createVector(0, 0, 0);
-        this.Distance = this.end - this.start;
-        this.Distance2 = this.end2 - this.start2;
-        this.accDist = this.Distance / this.distanceBoost;  // distance for acceleration and slow down
-        this.accDist2 = this.Distance2 / this.distanceBoost;  // distance for acceleration and slow down
+        this.Distance = this.end.sub(this.start);
+        this.DistanceLength = this.Distance.mag();
+        // this.Distance2 = this.end2 - this.start2;
+        this.accDist = this.DistanceLength / this.distanceBoost;  // distance for acceleration and slow down
+        // this.accDist2 = this.Distance2 / this.distanceBoost;  // distance for acceleration and slow down
         this.boost = this.fullspeed / this.accDist;
-        this.accA = this.start + this.accDist;  // distance for full speed
-        this.accA2 = this.start2 + this.accDist2;  // distance for full speed
-        this.accB = this.end - this.accDist;  // distance for slowing down speed
-        this.accB2 = this.end2 - this.accDist2;  // distance for slowing down speed
+        // this.accA = this.start + this.accDist;  // distance for full speed
+        // this.accA2 = this.start2 + this.accDist2;  // distance for full speed
+        this.accA = p5.Vector.add(this.start, p5.Vector.div(this.Distance, this.distanceBoost));  // distance for full speed
+        // this.accB = this.end - this.accDist;  // distance for slowing down speed
+        // this.accB2 = this.end2 - this.accDist2;  // distance for slowing down speed
+        this.accB = p5.Vector.add(this.start, p5.Vector.div(this.Distance, this.distanceBoost));  // distance for full speed
 
-        console.log("accdist: " + this.accDist);
-        console.log("boost: " + this.boost);
-        console.log("step: " + this.accDist / this.boost);
+        // console.log("accdist: " + this.accDist);
+        // console.log("boost: " + this.boost);
+        // console.log("step: " + this.accDist / this.boost);
 
 
         if (this.orientation == "x") {
@@ -76,28 +81,29 @@ class Brush {
         }
     }
 
-    move(mover, primaryAcc, secondaryAcc) {
+    // move(mover, primaryAcc, secondaryAcc) {
+    move() {
 
-        if (mover < primaryAcc) {
+        if (this.pos < this.accA) {
             // start
             this.acc = this.accBoost;
             // console.log("accelerate");
 
-        } else if (mover >= primaryAcc && mover < secondaryAcc) {
-            // full speed
-            this.acc = createVector(0, 0, 0);
-            // console.log("full speed");
+            // } else if (this.pos.x >= primaryAcc && this.pos.x < secondaryAcc) {
+            //     // full speed
+            //     this.acc = createVector(0, 0, 0);
+            //     // console.log("full speed");
 
-        } else if (mover >= (this.end - this.OkLevel)) {
+        } else if (this.pos >= p5.Vector.sub(this.end, this.OkLevel)) {
             // stop
             this.acc = createVector(0, 0, 0);
             this.vel = createVector(0, 0, 0);
             // console.log("stop");
             this.alive = false;  // reaching the goal of one axis is enough (xy & yx case)
-        } else if (mover >= secondaryAcc && mover < this.end) {
-            // slow down
-            this.acc = this.sloBoost;
-            // console.log("slow down");
+            // } else if (this.pos.x >= secondaryAcc && this.pos.x < this.end) {
+            //     // slow down
+            //     this.acc = this.sloBoost;
+            //     // console.log("slow down");
         }
         // console.log("pos x: " + mover + " end: " + this.end);
         this.vel.add(this.acc);
@@ -121,15 +127,60 @@ class Brush {
             this.move(this.pos.y, this.accA, this.accB);
         } else if (this.orientation == "xy" || this.orientation == "yx") {
             // this.move(this.pos.x, this.accA, this.accB);
-            this.move(this.pos.y, this.accA2, this.accB2);
+            // this.move(this.pos.y, this.accA2, this.accB2);
+            this.move();
         }
-
 
         this.radius = map(this.vel.x, 0, 3, 2.75, 1.75)
     }
 
     display() {
 
+        // start
+        push();
+        strokeWeight(3);
+        stroke(255, 255, 0);
+        translate(-width / 2, -height / 2);
+        translate(this.start);
+        // line(0, 0, this.Distance.x, this.Distance.y);
+        // line(0, 0, this.Distance.x / 4, this.Distance.y / 4);
+        fill("blue");
+        ellipse(0, 0, 10);
+        pop();
+
+
+        // accA
+        push();
+        translate(-width / 2, -height / 2);
+        // translate(this.accA);
+        noStroke();
+        fill("red");
+        ellipse(0, 0, 10);
+        stroke(5);
+        line(0, 0, this.start.x, this.start.y);
+        line(0, 0, this.end.x, this.end.y);
+        // line(0, 0, this.Distance.x, this.Distance.y);
+        pop();
+
+
+        // accB
+        // push();
+        // translate(-width / 2, -height / 2);
+        // translate(this.accB);
+        // noStroke();
+        // fill("red");
+        // ellipse(0, 0, 10);
+        // pop();
+
+
+        // end
+        push();
+        translate(-width / 2, -height / 2);
+        translate(this.end);
+        noStroke();
+        fill("purple");
+        ellipse(0, 0, 10);
+        pop();
 
         if (this.alive) {
 
@@ -151,49 +202,41 @@ class Brush {
             pop();
 
             if (MODE == 5) {
-                // start
-                push();
-                translate(-width / 2, -height / 2);
-                translate(this.start, this.pos.y, 0);
-                noStroke();
-                fill("purple");
-                ellipse(2);
-                pop();
+                //     // start
+                //     push();
+                //     translate(-width / 2, -height / 2);
+                //     translate(this.start, this.pos.y, 0);
+                //     noStroke();
+                //     fill("purple");
+                //     ellipse(2);
+                //     pop();
 
-                // accA
-                push();
-                translate(-width / 2, -height / 2);
-                translate(this.accA, this.pos.y, 0);
-                noStroke();
-                fill("red");
-                ellipse(2);
-                pop();
 
-                // accB
-                push();
-                translate(-width / 2, -height / 2);
-                translate(this.accB, this.pos.y, 0);
-                noStroke();
-                fill("red");
-                ellipse(2);
-                pop();
+                //     // accB
+                //     push();
+                //     translate(-width / 2, -height / 2);
+                //     translate(this.accB, this.pos.y, 0);
+                //     noStroke();
+                //     fill("red");
+                //     ellipse(2);
+                //     pop();
 
-                // line
-                push();
-                strokeWeight(1);
-                stroke("green");
-                translate(-width / 2, -height / 2);
-                line(this.start, 0, 0, this.end, 0, 0);
-                pop();
+                //     // line
+                //     push();
+                //     strokeWeight(1);
+                //     stroke("green");
+                //     translate(-width / 2, -height / 2);
+                //     line(this.start, 0, 0, this.end, 0, 0);
+                //     pop();
 
-                // end
-                push();
-                translate(-width / 2, -height / 2);
-                translate(this.end, this.pos.y, 0);
-                noStroke();
-                fill("purple");
-                ellipse(2);
-                pop();
+                //     // end
+                //     push();
+                //     translate(-width / 2, -height / 2);
+                //     translate(this.end, this.pos.y, 0);
+                //     noStroke();
+                //     fill("purple");
+                //     ellipse(2);
+                //     pop();
             }
         }
     }
